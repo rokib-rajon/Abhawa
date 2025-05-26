2c1301a31775f9b65ff9d46c849710de46a9a965
Rokib Rajon <55991864+rokib-rajon@users.noreply.github.com>
2025-05-26 21:53:53 +0600

    sitemap

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,15 +1,6 @@
 const fs = require('fs');
-const locations = [
-  { name: 'Dhaka', slug: 'dhaka' },
-  { name: 'Chattogram', slug: 'chattogram' },
-  { name: 'Khulna', slug: 'khulna' },
-  { name: 'Rajshahi', slug: 'rajshahi' },
-  { name: 'Barisal', slug: 'barisal' },
-  { name: 'Sylhet', slug: 'sylhet' },
-  { name: 'Rangpur', slug: 'rangpur' },
-  { name: 'Mymensingh', slug: 'mymensingh' }
-];
-const baseUrl = 'https://abhawa.com';
+
+const baseUrl = 'https://www.abhawa.com';
 const staticUrls = [
   '',
   '/about',
@@ -21,9 +12,7 @@ sitemap += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
 staticUrls.forEach(url => {
   sitemap += `  <url>\n    <loc>${baseUrl}${url}</loc>\n  </url>\n`;
 });
-locations.forEach(loc => {
-  sitemap += `  <url>\n    <loc>${baseUrl}/location/${loc.slug}</loc>\n  </url>\n`;
-});
+
 sitemap += '</urlset>';
 fs.writeFileSync('public/sitemap.xml', sitemap);
 console.log('Sitemap generated at public/sitemap.xml');
